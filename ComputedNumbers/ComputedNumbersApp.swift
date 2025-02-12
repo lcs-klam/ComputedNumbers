daeccfde9a80e4850ff8f479843f0d6797b902e8
Kevin Lam <klam@lcs.on.ca>
2025-02-12 09:03:12 -0500

    Initial Commit

---
+++ ComputedNumbers/ComputedNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputedNumbersApp.swift
+//  ComputedNumbers
+//
+//  Created by Kevin Lam on 12/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ComputedNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
