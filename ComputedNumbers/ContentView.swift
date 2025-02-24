78bdf52ae189ee3be8a2bafb81fe0848324a41b8
Kevin Lam <klam@lcs.on.ca>
2025-02-24 14:52:15 -0500

    Added Guard and IF statement to produce an error message if the parameters were not met

--- ComputedNumbers/ContentView.swift
+++ ComputedNumbers/ContentView.swift
@@ -12,14 +12,22 @@ struct ContentView: View {
     // MARK: Stored properties
     @State var number1: String = ""
     @State var number2: String = ""
-
+    @State var errorMessage: String = ""
     
     // MARK: Computed properties
-    var sumSquareRoot: Double {
-        return sqrt(Double(number1 + number2))
+    var sumSquareRoot: Double? {
+        if let num1 = Int(number1), num1 >= 0,
+           let num2 = Int(number2), num2 >= 0 {
+            errorMessage = ""
+            return sqrt(Double(num1 + num2))
+        } else {
+            errorMessage = "Please enter positive whole numbers."
+            return nil
+        }
+        
     }
       
-
+    
     
     
     var body: some View {
@@ -37,27 +45,33 @@ struct ContentView: View {
                 Text(") =")
                     .font(.system(size: 48))
                 
-                Text("\(sumSquareRoot.formatted(.number.precision(.significantDigits(2))))")
-                    .font(.system(size: 48))
+                if let result = sumSquareRoot {
+                    Text("\(result.formatted(.number.precision(.significantDigits(2))))")
+                        .font(.system(size: 48))
+                } else {
+                    Text(errorMessage)
+                        .font(.system(size: 10))
+                }
             }
             
             TextField("Enter number 1", text: $number1)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
-                .padding()
-
+            
+            
             TextField("Enter number 2", text: $number2)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
-                .padding()
-            }
-            .padding()
+            
             
             Spacer()
         }
-        .padding()
     }
 }
+
+
+
+
 
 #Preview {
     ContentView()
